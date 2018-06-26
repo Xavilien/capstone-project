396369eba496323aaba2fa33f90bcb96cfc4fb60
xavilien <xavilien@gmail.com>
2018-06-26 22:10:38 +0800

    Made more edits, problem with http request

--- src/app/asr/asr.service.ts
+++ src/app/asr/asr.service.ts
@@ -1,17 +1,5 @@
 import { Injectable } from '@angular/core';
-//import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HTTP } from "@ionic-native/http";
-
-/*
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Accept' : 'application/json',
-    'Content-Type':  "application/json; charset=UTF-8",
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
-    'Access-Control-Allow-Origin': '*',
-  })
-};
-*/
 
 @Injectable()
 export class SendASRService {
